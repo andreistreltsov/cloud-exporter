ca02425f6b58ad61ad1208a1f726eff93728314e
Andrei Streltsov <andrei.strel@gmail.com>
2015-10-31 22:17:55 +0300

    Implemented copying to shared location.

--- Program.fs
+++ Program.fs
@@ -2,28 +2,52 @@
 open System.Security.Permissions
 open System.IO
 
-let copyfile fullSourcePath fullDestination = 0
-let copyTestFileToTestDir = copyfile "" ""
 
+let printToConsole filePath = printfn "%s" filePath
 
-let initializeWathcherAt watchPath = 
-    let handleCreation = fun (x:FileSystemEventArgs) -> printfn "%s" (x.FullPath.ToString())
+let copyToDestination filePath destinationPath = 
+    File.Copy(filePath, Path.Combine(destinationPath, Path.GetFileName(filePath)), true)
+
+let remove filePath = 
+    File.Delete(filePath)
+
+let excludedExt = [| ".crdownloads" |]
+
+let isExportFile (fileName:string) = fileName.Contains(".x.") && (not(fileName.EndsWith(".crdownload")))
+
+let extractFileNameFrom filePath = Path.GetFileName(filePath)
+
+let createFileHandler destinationPath =
+    fun filePath -> 
+        if isExportFile (extractFileNameFrom filePath)
+        then
+            printToConsole filePath
+            copyToDestination filePath destinationPath
+            remove filePath
+
+let initializeWathcher watchPath fileHandler = 
+    let listener = fun (x:FileSystemEventArgs) -> fileHandler (x.FullPath.ToString())
     let fsWatcher = new FileSystemWatcher(watchPath)
-    fsWatcher.Created.Add(handleCreation)
+    fsWatcher.Created.Add(listener)
     fsWatcher.EnableRaisingEvents <- true 
     fsWatcher
 
-let specifiedOrDownloadsPath argv = 
+let sourcePath argv = 
     if Array.length(argv) > 1 
     then argv.[1] 
     else "/home/andrei/Downloads"
+
+let destinationPath argv = 
+    if Array.length(argv) > 2 
+    then argv.[2] 
+    else "/home/andrei/temp/cloudXporter"
 
 
 [<EntryPoint>]
 [<PermissionSet(SecurityAction.Demand, Name="FullTrust")>]
 let main argv = 
-    let watcher = initializeWathcherAt (specifiedOrDownloadsPath argv)
+    let fileHandler = createFileHandler (destinationPath argv)
+    let watcher = initializeWathcher (sourcePath argv) fileHandler
 
     let a = Console.ReadLine()
-    0 // return an integer exit code
-
+    0
