291b7f4c70466e0bf7fbbef83955af8436a29996
Andrei Streltsov <andrei.strel@gmail.com>
2015-11-03 07:45:53 +0300

    Refactoring

--- Program.fs
+++ Program.fs
@@ -2,52 +2,62 @@
 open System.Security.Permissions
 open System.IO
 
+type DirPath = DirPath of string
 
-let printToConsole filePath = printfn "%s" filePath
+module Configuration = 
+    type T = { Source:DirPath; Destination:DirPath }
 
-let copyToDestination filePath destinationPath = 
-    File.Copy(filePath, Path.Combine(destinationPath, Path.GetFileName(filePath)), true)
+    let create argv = 
+        if Array.length(argv) >= 3 
+        then Some {Source = DirPath(argv.[1]); Destination = DirPath(argv.[2])}
+        else None
 
-let remove filePath = 
-    File.Delete(filePath)
+module IncomingFile =
+    type T = IncomingFile of string
 
-let excludedExt = [| ".crdownloads" |]
+    let create filePath = IncomingFile(filePath)
 
-let isExportFile (fileName:string) = fileName.Contains(".x.") && (not(fileName.EndsWith(".crdownload")))
+    let name (IncomingFile filePath) = 
+        Path.GetFileName(filePath)
 
-let extractFileNameFrom filePath = Path.GetFileName(filePath)
+    let value (IncomingFile filePath) = filePath
 
-let createFileHandler destinationPath =
-    fun filePath -> 
-        if isExportFile (extractFileNameFrom filePath)
+    let copyTo (IncomingFile filePath) (DirPath directory) = 
+        File.Copy(filePath, Path.Combine(directory, filePath), true)
+
+    let isExportFile (file:T) = 
+        let fileName = name file
+        fileName.Contains(".x.") || fileName.EndsWith(".x") && (not(fileName.EndsWith(".crdownload")))&& (not(fileName.StartsWith(".")))
+
+    let printToConsole (file:T) = printfn "%s" (name file)
+
+    let remove (IncomingFile filePath) = 
+        File.Delete(filePath)
+
+
+let createFileHandler destination =
+    fun (file:IncomingFile.T) ->
+        if IncomingFile.isExportFile file
         then
-            printToConsole filePath
-            copyToDestination filePath destinationPath
-            remove filePath
+            IncomingFile.printToConsole file
+            IncomingFile.copyTo file destination
+            IncomingFile.remove file
 
-let initializeWathcher watchPath fileHandler = 
-    let listener = fun (x:FileSystemEventArgs) -> fileHandler (x.FullPath.ToString())
+let initializeWathcher (DirPath watchPath) fileHandler = 
     let fsWatcher = new FileSystemWatcher(watchPath)
-    fsWatcher.Created.Add(listener)
+    fsWatcher.Created.Add(fun(x:FileSystemEventArgs) -> fileHandler(IncomingFile.create(x.FullPath)))
     fsWatcher.EnableRaisingEvents <- true 
     fsWatcher
-
-let sourcePath argv = 
-    if Array.length(argv) > 1 
-    then argv.[1] 
-    else "/home/andrei/Downloads"
-
-let destinationPath argv = 
-    if Array.length(argv) > 2 
-    then argv.[2] 
-    else "/home/andrei/temp/cloudXporter"
-
 
 [<EntryPoint>]
 [<PermissionSet(SecurityAction.Demand, Name="FullTrust")>]
 let main argv = 
-    let fileHandler = createFileHandler (destinationPath argv)
-    let watcher = initializeWathcher (sourcePath argv) fileHandler
+    match (Configuration.create argv) with 
+        | Some c -> 
+            let watcher = createFileHandler c.Destination |> initializeWathcher c.Source
+            Console.ReadLine() |> ignore
 
-    let a = Console.ReadLine()
+        | None -> Console.WriteLine("Invalid usage")
     0
+
+
